d92d320260e72fc725178d25c2f727f0d78797b1
Abdulsalam-Aderoju <67542491+Abdulsalam-Aderoju@users.noreply.github.com>
2024-02-15 21:44:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+
+from fastapi import FastAPI
+
+app = FastAPI() 
+
+parameters = ["Density", "Viscosity", "Flash Point", "Heating Value"]
+
+@app.post("/blend-dashboard")
+async def blend_info(Density: float,
+                     Viscosity: float,
+                     Flash_Point: float,
+                     Heating_Value: float):
+
+    # Reset validity list and invalid parameters list for each request
+    validity = []
+    invalid_parameters = []
+
+    if not (775 <= Density <= 840):
+        validity.append("Invalid")
+        invalid_parameters.append("Density")
+
+    if not (0 <= Viscosity <= 8):
+        validity.append("Invalid")
+        invalid_parameters.append("Viscosity")
+
+    if not (Flash_Point >= 38):
+        validity.append("Invalid")
+        invalid_parameters.append("Flash Point")
+
+    if not (42 <= Heating_Value <= 43):
+        validity.append("Invalid")
+        invalid_parameters.append("Heating Value")
+
+    # Making judgments based on validity list
+    if "Invalid" not in validity:
+        return "Biofuel meets the requirements in Aviation Industry"
+    else:
+        invalid_parameters_str = ", ".join(invalid_parameters)
+        return f"The following parameters require adjustment: {invalid_parameters_str}"
